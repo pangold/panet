c270569243045dbf178060fa8d557d458bd0022b
pango <pangold@163.com>
2019-06-09 23:13:47 +0800

    clean up headers of net

---
+++ include/pan/net/rpc.hpp
@@ -0,0 +1,12 @@
+#ifndef __PAN_NET_RPC_HPP__
+#define __PAN_NET_RPC_HPP__
+
+#include <pan/net/rpc/server_handler.hpp>
+#include <pan/net/rpc/client.hpp>
+#include <pan/net/rpc/server.hpp>
+#include <pan/net/rpc/v2/client_handler.hpp>
+#include <pan/net/rpc/v2/server_handler.hpp>
+#include <pan/net/rpc/v2/client.hpp>
+#include <pan/net/rpc/v2/server.hpp>
+
+#endif // __PAN_NET_RPC_HPP__
